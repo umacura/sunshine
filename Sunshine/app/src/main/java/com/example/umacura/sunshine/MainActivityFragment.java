8eebdf47cd731bc6dac5ae45e6ca5e80fa490f93
Liliya Lementueva <l.lementyeva@gmail.com>
2015-06-07 21:49:18 -0700

    Fragment progress

--- Sunshine/app/src/main/java/com/example/umacura/sunshine/MainActivityFragment.java
+++ Sunshine/app/src/main/java/com/example/umacura/sunshine/MainActivityFragment.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -12,12 +17,32 @@ import android.view.ViewGroup;
  */
 public class MainActivityFragment extends Fragment {
 
+    private ArrayAdapter<String> adapter;
+
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        List<String> weekForecast = new ArrayList<String>();
+        weekForecast.add("Today-Sunny-88/63");
+        weekForecast.add("Tomorrow-Froggy-70/46");
+        weekForecast.add("Weds-Cloudy-72/63");
+        weekForecast.add("Thurs-Rainy-64/51");
+        weekForecast.add("Fri-Foggy-70/46");
+        weekForecast.add("Sat-Sunny-76/68");
+
+        adapter = new ArrayAdapter<String>(getActivity(),
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                weekForecast);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(adapter);
+
+        return rootView;
     }
 }
